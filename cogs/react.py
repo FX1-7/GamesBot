addbbf16b64fb75a62934ac2ff4216d1b79fd6e2
Keiran <keiranosullivan1712@gmail.com>
2021-08-14 22:06:15 +0100

    commit

--- cogs/react.py
+++ cogs/react.py
@@ -12,13 +12,12 @@ class Verification(commands.Cog):
     @commands.is_owner()
     async def verification(self, ctx, emoji, role: discord.Role):
 
-        emb = discord.Embed(colour=GREEN, title="How to verify",
-                            description="In order to verify please click the 👍 emoji below!\n\n"
-                                        "**Please make sure you've read the rules above before entering the server**")
+        emb = discord.Embed(colour=GREEN, title="Developer Access",
+                            description="In order to get developer access react with the 🎮 emoji below!\n\n")
         msg = await ctx.channel.send(embed=emb)
         await msg.add_reaction(emoji)
 
-        with open('./verification.json') as json_file:
+        with open('./React.json') as json_file:
             data = json.load(json_file)
 
             new_react_role = {'role_name': role.name,
@@ -28,16 +27,15 @@ class Verification(commands.Cog):
 
             data.append(new_react_role)
 
-        with open('./verification.json', 'w') as f:
+        with open('./React.json', 'w') as f:
             json.dump(data, f, indent=4)
-
 
     @commands.Cog.listener()
     async def on_raw_reaction_add(self, payload):
         if payload.member.bot:
             pass
         else:
-            with open('./verification.json') as react_file:
+            with open('./React.json') as react_file:
                 data = json.load(react_file)
                 for x in data:
                     if x['emoji'] == payload.emoji.name:
@@ -47,15 +45,16 @@ class Verification(commands.Cog):
 
     @commands.Cog.listener()
     async def on_raw_reaction_remove(self, payload):
-
-        with open('./verification.json') as react_file:
+        guild = await self.bot.fetch_guild(payload.guild_id)
+        member = await guild.fetch_member(payload.user_id)
+        with open('./React.json') as react_file:
             data = json.load(react_file)
             for x in data:
                 if x['emoji'] == payload.emoji.name:
                     role = discord.utils.get(self.bot.get_guild(
                         payload.guild_id).roles, id=x['role_id'])
 
-                    await self.bot.get_guild(payload.guild_id).get_member(payload.user_id).remove_roles(role)
+                    await member.remove_roles(role)
 
 def setup(bot):
     bot.add_cog(Verification(bot))
